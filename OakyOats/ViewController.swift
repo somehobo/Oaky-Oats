a764143607ce2c55c2995d00d1de495518ffd3b1
Nolan Brady <nolan@ferriel.com>
2020-06-02 13:59:57 -0700

    Initial Commit

---
+++ OakyOats/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OakyOats
+//
+//  Created by Nolan Brady on 6/2/20.
+//  Copyright © 2020 Nolan Brady. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
